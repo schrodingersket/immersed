60dceaf80b5982e502f48b38a01770942f0235bf
jonathan.thompson <jonathan@urbaneinnovation.com>
2020-04-13 17:54:33 -0600

    Update to 1.6.0.

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ VERSION_MODIFIER=
 NVIDIA_GPU=
 
 [[ "true" -eq "$USE_INTEL_MEDIA_SDK" ]] && VERSION_MODIFIER="-intel-media-sdk"
-[[ "true" -eq "$USE_NVIDIA_GPU" ]] && NVIDIA_GPU="--gpus $IMMERSED_NVIDIA_GPUS"
+#[[ "true" -eq "$USE_NVIDIA_GPU" ]] && NVIDIA_GPU="--gpus $IMMERSED_NVIDIA_GPUS"
 
 # Set xhost permissions
 #
@@ -21,7 +21,8 @@ docker run -ti \
 	   --name=immersed \
 	   --net=host \
 	   -v /tmp/.X11-unix:/tmp/.X11-unix \
+	   -v xorg.conf:/etc/X11/xorg.conf \
            -v immersed:/root \
-	   -e DISPLAY=unix$DISPLAY \
+           -e DISPLAY=unix$DISPLAY \
 	   $NVIDIA_GPU \
 	   schrodingersket/immersed:$IMMERSED_VERSION$VERSION_MODIFIER "$@"
